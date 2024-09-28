582c55cc0689d67a421c702de9f2c25ee001bd90
MSaidG <msgulberk@gmail.com>
2024-09-28 16:55:56 +0300

    Change click to hover

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
 (function() {
-   document.querySelector('.nav-button').addEventListener('click', function() {
-     this.parentNode.parentNode.classList.toggle('closed')
-   }, false);
+   document.querySelector('.nav-button').addEventListener('mouseenter', function(event) {
+    this.parentNode.parentNode.classList.toggle('closed')
+    event.stopPropagation()
+
+  }, false)
+
+  document.querySelector('.drop-down').addEventListener('mouseleave', function(event) {
+    this.classList = ('drop-down closed')
+    event.stopPropagation()
+
+  }, false)
+
 })();
