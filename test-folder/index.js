596ed8d704d77a8a3b7a92c312110ba4e0b419d5
MSaidG <msgulberk@gmail.com>
2024-09-29 22:27:14 +0300

    Update dropdown menu package

--- test-folder/index.js
+++ test-folder/index.js
@@ -9,11 +9,14 @@
 // const drpdwn_ = require('@msaidg/dropdown-menu')
 // const drpdwn = require('./node_modules/dropdown-menu-element')
 
-// import a from './node_modules/@msaidg/dropdown-menu'
-// import a from './node_modules/dropdown-menu-element'
+// import a from './script.js'
+import a from './node_modules/@msaidg/dropdown-menu/index.js'
 
+
+a.AddListItem("asdfasd")
+// import a from './node_modules/dropdown-menu-element'
 // const a = await import('./node_modules/@msaidg/dropdown-menu')
-const a = await import('./node_modules/dropdown-menu-element')
+// const a = await import('./node_modules/dropdown-menu-element')
 // CreateDropdown()
 // AddListItem("sfg")
 // AddListItem("sfgasdf")
